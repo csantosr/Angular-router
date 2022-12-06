7580e0d4c5950572342ceaac814e7f4e6091305e
Cristian Santos <csantosr@unal.edu.co>
2022-12-05 22:11:20 -0500

    Shared module!

--- src/app/website/website.module.ts
+++ src/app/website/website.module.ts
@@ -3,9 +3,6 @@ import { CommonModule } from '@angular/common';
 
 import { WebsiteRoutingModule } from './website-routing.module';
 
-import { ImgComponent } from './components/img/img.component';
-import { ProductComponent } from './components/product/product.component';
-import { ProductsComponent } from './components/products/products.component';
 import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './pages/home/home.component';
 import { CategoryComponent } from './pages/category/category.component';
@@ -16,17 +13,13 @@ import { RecoveryComponent } from './pages/recovery/recovery.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
 import { LayoutComponent } from './components/layout/layout.component';
-import { ReversePipe } from './pipes/reverse.pipe';
-import { TimeAgoPipe } from './pipes/time-ago.pipe';
-import { HighlightDirective } from './directives/highlight.directive';
+
 import { SwiperModule } from 'swiper/angular';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
   declarations: [
-    ImgComponent,
-    ProductComponent,
-    ProductsComponent,
     NavComponent,
     HomeComponent,
     CategoryComponent,
@@ -37,14 +30,12 @@ import { SwiperModule } from 'swiper/angular';
     ProfileComponent,
     ProductDetailComponent,
     LayoutComponent,
-    ReversePipe,
-    TimeAgoPipe,
-    HighlightDirective,
   ],
   imports: [
     CommonModule,
     WebsiteRoutingModule,
-    SwiperModule
+    SwiperModule,
+    SharedModule
   ]
 })
 export class WebsiteModule { }
