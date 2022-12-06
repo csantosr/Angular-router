9bf9d76655aa8428c52dd2c18fb43589d7493d3e
Cristian Santos <csantosr@unal.edu.co>
2022-12-05 20:49:22 -0500

    Categories list

--- src/app/pages/category/category.component.ts
+++ src/app/pages/category/category.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+import { Product } from 'src/app/models/product.model';
+import { ProductsService } from 'src/app/services/products.service';
 
 @Component({
   selector: 'app-category',
-  templateUrl: './category.component.html',
+  template: `<app-products [products]="products" (sendMore)="loadMore()"></app-products>`,
   styleUrls: ['./category.component.scss']
 })
 export class CategoryComponent implements OnInit {
+  products: Product[] = [];
+  categoryId: string | null = null;
+  limit = 10;
+  offset = 0;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private productsService: ProductsService
+  ) { }
 
   ngOnInit(): void {
+    this.route.paramMap
+    .pipe(
+      switchMap((params) => {
+        this.categoryId = params.get('id');
+        if (this.categoryId) {
+          return this.productsService.getByCategory(this.categoryId, this.limit, this.offset)
+        }
+        return [];
+      })
+    )
+    .subscribe((data) => {
+      this.products = data;
+    })
+  }
+
+  loadMore() {
+    this.productsService.getAll(this.limit, this.offset).subscribe((data) => {
+      this.products = this.products.concat(data);
+      this.offset += this.limit;
+    });
   }
 
 }
