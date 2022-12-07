0344196292545aa5694d96b0282cc7d5444ad33a
Cristian Santos <csantosr@unal.edu.co>
2022-12-06 21:41:45 -0500

    En el estado

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { AuthService } from '../services/auth.service';
 import { TokenService } from '../services/token.service';
 
 @Injectable({
@@ -8,20 +10,30 @@ import { TokenService } from '../services/token.service';
 })
 export class AuthGuard implements CanActivate {
 
-  constructor (
-    private tokenService: TokenService,
+  constructor(
+    private authService: AuthService,
     private router: Router,
-  ) {}
+  ) { }
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot,
   ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    const token = this.tokenService.getToken();
-    if (!token) {
-      this.router.navigate(['/home']);
-      return false
-    }
-    return true;
+    // const token = this.tokenService.getToken();
+    // if (!token) {
+    //   this.router.navigate(['/home']);
+    //   return false
+    // }
+    // return true;
+    return this.authService.user$
+      .pipe(
+        map((user) => {
+          if (!user) {
+            this.router.navigate(['/home']);
+            return false
+          }
+          return true;
+        })
+      );
   }
 
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { switchMap, tap } from 'rxjs/operators';
 
 import { environment } from './../../environments/environment';
 import { Auth } from './../models/auth.model';
 import { User } from './../models/user.model';
 import { TokenService } from './../services/token.service';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +14,9 @@ import { TokenService } from './../services/token.service';
 export class AuthService {
 
   private apiUrl = `${environment.API_URL}/api/auth`;
+  private user = new BehaviorSubject<User | null>(null);
+
+  user$ = this.user.asObservable();
 
   constructor(
     private http: HttpClient,
@@ -22,12 +26,15 @@ export class AuthService {
   login(email: string, password: string) {
     return this.http.post<Auth>(`${this.apiUrl}/login`, {email, password})
     .pipe(
-      tap(response => this.tokenService.saveToken(response.access_token))
+      tap(response => this.tokenService.saveToken(response.access_token)),
     );
   }
 
   getProfile() {
-    return this.http.get<User>(`${this.apiUrl}/profile`);
+    return this.http.get<User>(`${this.apiUrl}/profile`)
+      .pipe(
+        tap(data => this.user.next(data))
+      );
   }
 
   loginAndGet(email: string, password: string) {
